6e55769d1dabd210e036c7f2cda7858191a16faa
Alan Gao <agao1@bayviewglen.ca>
2021-09-14 20:39:59 -0400

    homework

---
+++ Unit One Introduction to CS/src/Week2/UnitOneHomework.java
@@ -0,0 +1,72 @@
+package Week2;
+
+public class UnitOneHomework {
+    public static void main(String[] args) {
+        //Question 1
+        //double x = 3.5;
+        //System.out.println("Original number: "+x);
+        //System.out.println("Squared number: "+Math.pow(x, 2));
+
+        //Question 2
+        //double TotalScore = 82;
+        //double Score = 68;
+        //System.out.println("Percentage: "+(Score/TotalScore)*100);
+
+        //Question 3
+        //int num = 83749;
+        //int SecondNum = (num%10000)/1000;
+        //int FourthNum = (((num%10000)%1000)%100)/10;
+        //System.out.println(SecondNum*FourthNum);
+
+        //Question 4
+        //double Vi = 6.4;
+        //double a = 2.3;
+        //double t = 1.7;
+        //System.out.println(Vi+(a*t));
+
+        //Question 5
+        //double r = 4;
+        //double a = Math.PI*(Math.pow(r, 2));
+        //System.out.println(a);
+
+        //Question 6
+        //double r = 4;
+        //double v = (4.0/3.0)*Math.PI*Math.pow(r, 3);
+        //System.out.println(v);
+
+        //Question 7
+        // int a = 3;
+        // int b = 2;
+        // int c = 8;
+        // int x = 5;
+        // double y = a*Math.pow(x, 2)+b*x+c;
+        // System.out.println(y);
+
+        // Question 8
+        // int x1 = 4;
+        // int x2 = 5;
+        // int y1 = 0;
+        // int y2 = 3;
+        // double slope = (y2-y1)/(x2-x1);
+        // System.out.print(slope);
+
+        //Question 9
+        // double p = 34;
+        // double n = 13;
+        // double d = 3;
+        // double q = 14;
+        // double l = 3;
+        // double t = 2;
+        // double money = (p*0.01)+(n*0.05)+(d*0.1)+(q*0.25)+l+(t*2);
+        // System.out.println(money);
+
+        //Question 10
+        // double a = -3;
+        // double b = 2;
+        // double c = 8;
+        // double x1= ((0-b)+Math.sqrt(Math.pow(b, 2)-4*a*c))/2*a;
+        // double x2= ((0-b)-Math.sqrt(Math.pow(b, 2)-4*a*c))/2*a;
+        // System.out.println(x1);
+        // System.out.println(x2);
+    }
+}
