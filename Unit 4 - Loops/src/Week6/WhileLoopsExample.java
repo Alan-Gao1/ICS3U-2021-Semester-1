897a6aae9f13a6f45e2a5d3e862b814e58009ccb
Alan Gao <agao1@bayviewglen.ca>
2021-10-08 09:28:58 -0400

    unit 4

---
+++ Unit 4 - Loops/src/Week6/WhileLoopsExample.java
@@ -0,0 +1,38 @@
+package Week6;
+
+import java.util.Scanner;
+
+public class WhileLoopsExample {
+    public static void main(String[] args) {
+        exampleOne();
+        int sum = getSumInclusive(1, 9);
+        System.out.println(sum);
+        int numVowels = countVowels("This is a sentence with VOWELS!");
+        System.out.println(numVowels);
+  
+        Scanner in = new Scanner(System.in);
+        int num = getValidInput(10, 20);
+  
+     }
+  
+     private static int getValidInput(int i, int j) {
+        return 0;
+     }
+  
+     private static int getSumInclusive(int i, int j) {
+        return 0;
+     }
+  
+     private static int countVowels(String string) {
+        return 0;
+     }
+  
+     private static void exampleOne() {
+        //sum the numbers from 1 to 100 and display the sun
+        int count = 1;
+        int sum = 0;
+        while(count<=100){
+            
+        }
+     }
+  }
---
+++ Unit Three - Selection and If statements/src/Week5/Week6/CompareToExample.java
@@ -0,0 +1,10 @@
+package Week5.Week6;
+
+public class CompareToExample {
+    public static void main(String[] args) {
+        System.out.println("abc".compareTo("bcde"));
+        System.out.println("a".compareTo("g"));
+        System.out.println("g".compareTo("a"));
+        System.out.println("g".compareTo("g"));
+    }
+}
